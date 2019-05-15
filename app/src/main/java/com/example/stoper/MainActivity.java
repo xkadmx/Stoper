f64da2afcaa3aaa93aeb21e1c30a2297052506b0
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-15 22:33:11 +0200

    adding savedInstance method to prevent data loss if the configuration changes by capturing values from two variables (seconds, running)

--- app/src/main/java/com/example/stoper/MainActivity.java
+++ app/src/main/java/com/example/stoper/MainActivity.java
@@ -14,6 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState !=null){
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+        }
 
         runTimer();
     }
