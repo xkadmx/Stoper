be583a12d1f406d7e2a0fae9ebff452811a36e32
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-15 20:55:10 +0200

    preparing activity layout: changing from Constrained to Relative, and setting parameters for TextView and three buttons to serve onClickStart, onClickStop and onClickReset methods

--- app/src/main/java/com/example/stoper/MainActivity.java
+++ app/src/main/java/com/example/stoper/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.stoper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickStart(View view) {
+    }
+
+    public void onClickStop(View view) {
     }
 }
