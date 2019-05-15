4af3020c63f873bae0338fe1c71d5c100e665f64
xkadmx <zuzanna.rosinska@yahoo.com>
2019-05-15 21:08:29 +0200

    adding code serving the widgets activity into main class most importantly declaring second and running variable to contain seconds passed and information is the stoper is due or not

--- app/src/main/java/com/example/stoper/MainActivity.java
+++ app/src/main/java/com/example/stoper/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private int seconds = 0;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,11 +15,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickStart(View view) {
+        running = true;
     }
 
     public void onClickStop(View view) {
+        running = false;
     }
 
     public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
     }
+
 }
